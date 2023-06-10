74c53e63bf7b23931023c3f85060f2526f30525c
Cheilyn Coronado <115672054+China-7@users.noreply.github.com>
2023-06-10 13:20:28 -0400

    Add files via upload

---
+++ paquete/calculos.py
@@ -0,0 +1,8 @@
+def suma(n ,n1):
+      print("El resultado de la suma es",n+n1)
+def resta(n ,n1):
+      print("El resultado de la resta es",n-n1)
+def multiplica(n ,n1):
+      print("El resultado de la multiplica es",n*n1)
+def  potenciacion(n ,n1):
+      print("El resultado de la  potenciacion es",n**n1)
