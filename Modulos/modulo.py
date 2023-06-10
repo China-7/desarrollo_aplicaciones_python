294c89b9ebd31e131a29a73ffcbf369c0368b053
Cheilyn Coronado <115672054+China-7@users.noreply.github.com>
2023-06-10 13:18:34 -0400

    Add files via upload

---
+++ Modulos/modulo.py
@@ -0,0 +1,13 @@
+#Crear modulos en python
+
+def suma(a,b):
+      return a+b
+
+def resta (a,b):
+      return a-b
+
+def multi (a,b):
+      return a*b
+
+def divi (a,b):
+      return a/b
---
+++ Modulos/modulo1.py
@@ -0,0 +1,10 @@
+# Importar modulos
+
+import modulo
+
+print(modulo.multi(4,2))
+print(modulo.divi(8,2 ))
+
+from modulo import suma , resta
+print("Este es el resultado de la funcion  suma", suma(4,7))
+print("Este es el resultado de la funcion resta ", resta(4,8))
